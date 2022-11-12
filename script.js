4aa51d23f99ced0e0a811b0d61f18eb366e607eb
Pmint 05 <phucon547@gmail.com>
2022-11-12 13:02:23 +0700

    init

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-const paths = document.querySelectorAll("#svg svg path");
-for (let path of paths) {
-	console.log(path.getTotalLength());
-}
-function run() {
+const run = () => {
 	document.querySelector("#texts").classList.add("active");
 	document
 		.querySelector("#texts span:last-child")
 		.addEventListener("animationend", () => {
 			document.querySelector("#svg").classList.add("active", "loaded");
 		});
-}
+};
 document.addEventListener("DOMContentLoaded", () => {
 	run();
 });
@@ -18,7 +14,7 @@ document.querySelector(".btn-reset").onclick = () => {
 	document.querySelector("#texts").classList.remove("active");
 	document.querySelector("#svg").classList.remove("active", "loaded");
 	document.querySelector("#texts").style.animation = "none";
-	document.querySelector("#texts").offsetHeight; /* trigger reflow */
+	document.querySelector("#texts").offsetHeight;
 	document.querySelector("#texts").style.animation = null;
 	run();
 };
